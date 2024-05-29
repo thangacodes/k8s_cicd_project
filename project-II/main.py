edb0e548dcc708f560db83e61d37ee44b8779cd6
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-05-29 11:44:56 +0530

    Update main.py

--- project-II/main.py
+++ project-II/main.py
@@ -1,7 +1,10 @@
-FROM python:3.9  
-WORKDIR /code
-COPY ./requirements.txt /code/requirements.txt
-RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
-COPY ./main.py /code/main.py
-COPY ./form.html /code/form.html
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
+#uvicorn main:app --reload
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="/code")
+
+@app.get("/")
+def form_post(request: Request):
+    return templates.TemplateResponse('aws.html', context={'request': request})
